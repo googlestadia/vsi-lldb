13c79bec1bab9a38095f01ca34523758b2338ff8
Lutz Justen <ljusten@google.com>
2022-12-19 10:23:52 +0100

    [YetiCommon] Rename test binaries
    Adds .dat to test binary names, which should prevent accidental
    execution. Note that the tests don't actually execute the binaries,
    but rather just parse the header info.
    
    GitOrigin-RevId: dc3dbbafa9c619b8c062a2444951cff8c3f5a2cb

--- YetiCommon.Tests/ModuleParserTests.ParseBuildIdInfo.cs
+++ YetiCommon.Tests/ModuleParserTests.ParseBuildIdInfo.cs
@@ -5,16 +5,22 @@ namespace YetiCommon.Tests
 {
     public partial class ModuleParserTests
     {
-        [TestCase("hello_cpp.exe", ModuleFormat.Pe, "DD2CC932-5E32-4D66-8B03-875CC0965322-00000001")]
-        [TestCase("hello_cpp.pdb", ModuleFormat.Pdb, "DD2CC932-5E32-4D66-8B03-875CC0965322-00000001")]
-        [TestCase("hello", ModuleFormat.Elf, "7119D4F0-85EB-2938-0EAF-23B40BA54FC1-73BF3747")]
-        [TestCase("hello_dotnet.exe", ModuleFormat.Pe, "445A5B1C-1EF1-4B03-8193-7A4B770873AB-00000001")]
-        [TestCase("hello_dotnet.pdb", ModuleFormat.Pdb, "FDF485D6-E134-4825-885F-03517602265D-00000001")]
-        [TestCase("hello_dotnet.dll", ModuleFormat.Pe, "FDF485D6-E134-4825-885F-03517602265D-00000001")]
+        [TestCase("hello_cpp_exe.dat", ModuleFormat.Pe,
+                  "DD2CC932-5E32-4D66-8B03-875CC0965322-00000001")]
+        [TestCase("hello_cpp_pdb.dat", ModuleFormat.Pdb,
+                  "DD2CC932-5E32-4D66-8B03-875CC0965322-00000001")]
+        [TestCase("hello_elf.dat", ModuleFormat.Elf,
+                  "7119D4F0-85EB-2938-0EAF-23B40BA54FC1-73BF3747")]
+        [TestCase("hello_dotnet_exe.dat", ModuleFormat.Pe,
+                  "445A5B1C-1EF1-4B03-8193-7A4B770873AB-00000001")]
+        [TestCase("hello_dotnet_pdb.dat", ModuleFormat.Pdb,
+                  "FDF485D6-E134-4825-885F-03517602265D-00000001")]
+        [TestCase("hello_dotnet_dll.dat", ModuleFormat.Pe,
+                  "FDF485D6-E134-4825-885F-03517602265D-00000001")]
         public void ParseBuildId_Succeeds(string filename, ModuleFormat format, string buildId)
         {
-            string path = Path.Combine(TestContext.CurrentContext.TestDirectory, "binaries",
-                                       filename);
+            string path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData",
+                                       "ModuleParserTests", filename);
             var moduleParser = new ModuleParser();
             BuildIdInfo output = moduleParser.ParseBuildIdInfo(path, format);
 
@@ -25,26 +31,26 @@ namespace YetiCommon.Tests
         [Test]
         public void ParseBuildId_FileNotFound([Values] ModuleFormat format)
         {
-            string path = Path.Combine(TestContext.CurrentContext.TestDirectory, "binaries",
-                                       "unknown_file");
+            string path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData",
+                                       "ModuleParserTests", "unknown_file");
             var moduleParser = new ModuleParser();
             BuildIdInfo output = moduleParser.ParseBuildIdInfo(path, format);
 
             Assert.That(output.Error.EndsWith("unknown_file not found"));
         }
 
-        [TestCase("hello_cpp.exe", ModuleFormat.Elf)]
-        [TestCase("hello_cpp.exe", ModuleFormat.Pdb)]
-        [TestCase("hello_cpp.pdb", ModuleFormat.Elf)]
-        [TestCase("hello_cpp.pdb", ModuleFormat.Pe)]
-        [TestCase("hello", ModuleFormat.Pe)]
-        [TestCase("hello", ModuleFormat.Pdb)]
-        [TestCase("hello_dotnet.dll", ModuleFormat.Elf)]
-        [TestCase("hello_dotnet.dll", ModuleFormat.Pdb)]
+        [TestCase("hello_cpp_exe.dat", ModuleFormat.Elf)]
+        [TestCase("hello_cpp_exe.dat", ModuleFormat.Pdb)]
+        [TestCase("hello_cpp_pdb.dat", ModuleFormat.Elf)]
+        [TestCase("hello_cpp_pdb.dat", ModuleFormat.Pe)]
+        [TestCase("hello_elf.dat", ModuleFormat.Pe)]
+        [TestCase("hello_elf.dat", ModuleFormat.Pdb)]
+        [TestCase("hello_dotnet_dll.dat", ModuleFormat.Elf)]
+        [TestCase("hello_dotnet_dll.dat", ModuleFormat.Pdb)]
         public void ParseBuildId_IncorrectFormat(string filename, ModuleFormat format)
         {
-            string path = Path.Combine(TestContext.CurrentContext.TestDirectory, "binaries",
-                                       filename);
+            string path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData",
+                                       "ModuleParserTests", filename);
             var moduleParser = new ModuleParser();
             BuildIdInfo output = moduleParser.ParseBuildIdInfo(path, format);
 
---
+++ YetiCommon.Tests/TestData/ModuleParserTests/README.md
@@ -0,0 +1,5 @@
+# ModuleParserTests binaries
+
+The ModuleParserTests parse different binary test files. To prevent detection
+of binaries in the code base, and accidental execution, the files have been
+named `.dat`.
